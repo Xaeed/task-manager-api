e55beb9aeae9ca8032082aa8a5f8b4f308b2eff0
Saeed Butt <saeed.butt@aboutyou.de>
2020-01-14 01:03:51 +0100

    fix node environment variables

--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ require('./db/mongoose')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
+const dotnet=require('dotenv')
+
+dotnet.config({
+    path:'src/config/development.env'
+})
+
 const app = express()
-const port = process.env.PORT 
+const port = process.env.PORT  || 30000
 
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
 app.listen(port, () => {
-    console.log('Server is up on port ' + port)
+    console.log('Server is up on port ' ,port)
 })
